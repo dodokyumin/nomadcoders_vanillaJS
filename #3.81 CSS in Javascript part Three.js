bf33688ce9b1093e276ddde1926011c2aef036ce
dodokyumin <95400620+dodokyumin@users.noreply.github.com>
2022-01-25 12:42:10 +0900

    Add files via upload

--- #3.81 CSS in Javascript part Three.js
+++ #3.81 CSS in Javascript part Three.js
@@ -5,3 +5,6 @@ function handleH1Click() {
 }
 
 H1.addEventListener("click", handleH1Click);
+
+//toggle 함수는 H1의 classList에 clicked class가 이미 있는지 확인해서 만약 있다면, toggle이 clicked를 제거해준다. 만약 없다면 clicked를 classList에 추가해준다.
+//마치 스마트폰 홀드키처럼.
